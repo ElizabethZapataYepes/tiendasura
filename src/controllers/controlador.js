80995d2b764909033e142202c8891d45ad960a7d
LorenaPosada <407@academicos.cesde.edu.co>
2022-07-21 17:41:22 -0500

    creando tienda

---
+++ src/controllers/controlador.js
@@ -0,0 +1,11 @@
+//creando variables 
+
+let nombreusuario="Elizabeth Zapata"
+const EDAD_USUARIO=29
+let estatura=1.55
+let hinchaDelVerde=true
+
+console.log(nombreusuario)
+
+
+console.log(EDAD_USUARIO)
